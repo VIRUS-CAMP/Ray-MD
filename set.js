e26c7dfe97c638e2c0570a5de3ce5ad96f92fba7
VIRUS-CAMP <raymondkimath630@gmail.com>
2025-08-01 21:37:26 +0300

    Update set.js

--- set.js
+++ set.js
@@ -7,12 +7,12 @@ const databasePath = path.join(__dirname, './database.db');
 const DATABASE_URL = process.env.DATABASE_URL === undefined
     ? databasePath
     : process.env.DATABASE_URL;
-module.exports = { session: process.env.SESSION_ID || 'ezra',
+module.exports = { session: process.env.SESSION_ID || 'ray',
     PREFIXE: process.env.PREFIX || "+",
-    GITHUB : process.env.GITHUB|| 'https://github.com/mr-X-force/LUCKY-MD-XFORCE',
-    OWNER_NAME : process.env.OWNER_NAME || "FrediEzra",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || "255752593977",
-    DEV : process.env.DEV || "FrediEzra Tz",
+    GITHUB : process.env.GITHUB|| 'https://github.com/VIRUS-CAMP/Ray-MD',
+    OWNER_NAME : process.env.OWNER_NAME || "Shukraniray",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || "255773350309",
+    DEV : process.env.DEV || "Shukraniray Tz",
               
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "non",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'non',
@@ -26,14 +26,14 @@ module.exports = { session: process.env.SESSION_ID || 'ezra',
     AUTO_BLOCK: process.env.AUTO_BLOCK || 'no', 
     GCF: process.env.GROUP_HANDLE || 'no', 
     AUTO_REPLY : process.env.AUTO_REPLY || "no", 
-    AUTO_STATUS_TEXT: process.env.AUTO_STATUS_TEXT || 'Your Status Seen By LUCKY-MD-XFORCE',   
+    AUTO_STATUS_TEXT: process.env.AUTO_STATUS_TEXT || 'Your Status Seen By Ray-MD',   
     AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || 'no',
     AUTO_BIO: process.env.AUTO_BIO || 'yes',       
     ANTI_CALL_TEXT : process.env.ANTI_CALL_TEXT || '',             
     GURL: process.env.GURL  || "https://whatsapp.com/channel/0029VaihcQv84Om8LP59fO3f",
     WEBSITE :process.env.GURL || "https://whatsapp.com/channel/0029VaihcQv84Om8LP59fO3f",
-    CAPTION : process.env.CAPTION || "LUCKY-MD-XFORCE",
-    BOT : process.env.BOT_NAME || 'LUCKY-MD-XFORCE',
+    CAPTION : process.env.CAPTION || "Ray-MD",
+    BOT : process.env.BOT_NAME || 'Ray-MD',
     MODE: process.env.PUBLIC_MODE || "no",              
     TIMEZONE: process.env.TIMEZONE || "Africa/Dodoma", 
     PM_PERMIT: process.env.PM_PERMIT || 'no',
@@ -42,7 +42,7 @@ module.exports = { session: process.env.SESSION_ID || 'ezra',
     WARN_COUNT : process.env.WARN_COUNT || '5' ,
     ETAT : process.env.PRESENCE || '1',
     DP : process.env.STARTING_BOT_MESSAGE || "yes",
-    LUCKY_ADM : process.env.ANTI_DELETE_MESSAGE || 'no',
+    RAY_ADM : process.env.ANTI_DELETE_MESSAGE || 'no',
     ANTI_DELETE_GROUP : process.env.ANTI_DELETE_GROUP || 'no',
     ANTI_CALL: process.env.ANTI_CALL || 'yes', 
     AUDIO_REPLY : process.env.AUDIO_REPLY || 'yes', 
@@ -51,21 +51,7 @@ module.exports = { session: process.env.SESSION_ID || 'ezra',
     DATABASE_URL,
     DATABASE: DATABASE_URL === databasePath
         ? "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9" : "postgres://db_7xp9_user:6hwmTN7rGPNsjlBEHyX49CXwrG7cDeYi@dpg-cj7ldu5jeehc73b2p7g0-a.oregon-postgres.render.com/db_7xp9",
-    /* new Sequelize({
-     dialect: 'sqlite',
-     storage: DATABASE_URL,
-     logging: false,
-})
-: new Sequelize(DATABASE_URL, 
-     dialect: 'postgres',
-     ssl: true,
-     protocol: 'postgres',
-     dialectOptions: {
-         native: true,
-         ssl: { require: true, rejectUnauthorized: false },
-     },
-     logging: false,
-}),*/
+    
 };
 let fichier = require.resolve(__filename);
 fs.watchFile(fichier, () => {
